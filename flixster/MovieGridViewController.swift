2a2c5707188d390d56b204a601c9f13510df892c
Carla Rose <57645881+carlarose01@users.noreply.github.com>
2021-02-26 21:14:05 -0600

    Implemented superhero detail screen

--- flixster/MovieGridViewController.swift
+++ flixster/MovieGridViewController.swift
@@ -80,11 +80,25 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // In a storyboard-based application, you will often want to do a little preparation before navigation*/
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//          Get the new view controller using segue.destination.
+//          Pass the selected object to the new view controller.
+         
+         print("Loading details screen")
+         
+//          Find selected movie
+         let cell = sender as! UICollectionViewCell
+         let indexPath = collectionView.indexPath(for: cell)
+         let movie = movies[indexPath!.item]
+         
+//          Pass selected movie ot detials view controller
+         let detailsViewController = segue.destination as! SuperheroDetailsViewController
+         detailsViewController.movie = movie
+         
+        collectionView.deselectItem(at: indexPath!, animated: true)
+        
+     }
+    
 
 }
